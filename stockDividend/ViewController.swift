06f3a6af43922c4cd2918d7a75ae32292e666ad1
NomikLee <nomiklee@icloud.com>
2023-07-26 20:09:12 +0800

    Initial Commit

---
+++ stockDividend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockDividend
+//
+//  Created by Pinocchio on 2023/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
